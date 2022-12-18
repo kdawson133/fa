07fb35ce939a2d765bcd8dccd636a022816846df
kdawson133 <kdawson133@gmail.com>
2022-12-18 12:32:13 +1000

    added

---
+++ functions/grr.fish
@@ -0,0 +1,15 @@
+# grr.fish
+#  _       _                               __ ____ ____  
+# | |     | |                             /_ |___ \___ \ 
+# | | ____| | __ ___      _____  ___  _ __ | | __) |__) |
+# | |/ / _` |/ _` \ \ /\ / / __|/ _ \| '_ \| ||__ <|__ < 
+# |   < (_| | (_| |\ V  V /\__ \ (_) | | | | |___) |__) |
+# |_|\_\__,_|\__,_| \_/\_/ |___/\___/|_| |_|_|____/____/ 
+#
+# https://github.com/kdawson133/
+#
+# @kirk133
+#           
+function grr --wraps='git remote rm' --description 'alias grr git remote rm'
+  git remote rm $argv; 
+end
