546441a043feb83887cfe6ccd8aa9b742bb8dfc2
kdawson133 <kdawson133@gmail.com>
2022-12-18 12:39:05 +1000

    added

---
+++ functions/lt.fish
@@ -0,0 +1,15 @@
+# lt.fish
+#  _       _                               __ ____ ____  
+# | |     | |                             /_ |___ \___ \ 
+# | | ____| | __ ___      _____  ___  _ __ | | __) |__) |
+# | |/ / _` |/ _` \ \ /\ / / __|/ _ \| '_ \| ||__ <|__ < 
+# |   < (_| | (_| |\ V  V /\__ \ (_) | | | | |___) |__) |
+# |_|\_\__,_|\__,_| \_/\_/ |___/\___/|_| |_|_|____/____/ 
+#
+# https://github.com/kdawson133/
+#
+# @kirk133
+#           
+function lt --wraps='exa -T --level=3 --icons' --description 'alias lt exa -T --level=3 --icons'
+  exa -T --level=3 --icons $argv; 
+end
