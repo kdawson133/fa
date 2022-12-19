f569e9994f3ec37cd7abeb3b99bcdf97aad4d6ba
kdawson133 <kdawson133@gmail.com>
2022-12-19 19:24:20 +1000

    added

---
+++ conf.d/ffa.fish
@@ -0,0 +1,46 @@
+# mfa.fish
+#  _       _                               __ ____ ____  
+# | |     | |                             /_ |___ \___ \ 
+# | | ____| | __ ___      _____  ___  _ __ | | __) |__) |
+# | |/ / _` |/ _` \ \ /\ / / __|/ _ \| '_ \| ||__ <|__ < 
+# |   < (_| | (_| |\ V  V /\__ \ (_) | | | | |___) |__) |
+# |_|\_\__,_|\__,_| \_/\_/ |___/\___/|_| |_|_|____/____/ 
+#
+# https://github.com/kdawson133/
+#
+# @kirk133
+#         
+# find aliases
+#
+# call fd instead of find
+function find
+	fd $argv;
+end
+# fd pattern
+function fd
+	fd -IHiaL $argv;
+end
+# fd extention
+function fde
+	fd -e $argv;
+end
+# fd type file
+function fdtf
+	fd -t f $argv;
+end
+# fd type directory
+function fdtd
+	fd -t d $argv;
+end
+# fd type symlink
+function fdtl
+	fd -t l $argv;
+end
+# fd type executable
+function fdtx
+	fd -t x $argv;
+end
+# fd type empty
+function fdte
+	fd -t e $argv;
+end
