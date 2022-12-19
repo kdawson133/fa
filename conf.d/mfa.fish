455c3091e9c6520599b2c7b797defd183024987e
kdawson133 <kdawson133@gmail.com>
2022-12-19 13:57:41 +1000

    added

---
+++ conf.d/mfa.fish
@@ -0,0 +1,26 @@
+# mfa.fish
+#  _       _                               __ ____ ____  
+# | |     | |                             /_ |___ \___ \ 
+# | | ____| | __ ___      _____  ___  _ __ | | __) |__) |
+# | |/ / _` |/ _` \ \ /\ / / __|/ _ \| '_ \| ||__ <|__ < 
+# |   < (_| | (_| |\ V  V /\__ \ (_) | | | | |___) |__) |
+# |_|\_\__,_|\__,_| \_/\_/ |___/\___/|_| |_|_|____/____/ 
+#
+# https://github.com/kdawson133/
+#
+# @kirk133
+#         
+# git aliases
+#
+function :q
+  exit; 
+end
+function :c
+  clear; 
+end
+function vim
+  git nvim $argv; 
+end
+function svim
+  sudo nvim $argv; 
+end
