b32ca12c3efbb17d1d76249cbb46fab9b2e12c87
kdawson133 <kdawson133@gmail.com>
2022-12-20 09:40:19 +1000

    added

---
+++ functions/ex.fish
@@ -0,0 +1,52 @@
+# afa.fish
+#  _       _                               __ ____ ____  
+# | |     | |                             /_ |___ \___ \ 
+# | | ____| | __ ___      _____  ___  _ __ | | __) |__) |
+# | |/ / _` |/ _` \ \ /\ / / __|/ _ \| '_ \| ||__ <|__ < 
+# |   < (_| | (_| |\ V  V /\__ \ (_) | | | | |___) |__) |
+# |_|\_\__,_|\__,_| \_/\_/ |___/\___/|_| |_|_|____/____/ 
+#
+# https://github.com/kdawson133/
+#
+# @kirk133
+#         
+# archive extraction aliases
+#
+function ex
+  if test -f $argv
+		switch $argv
+    	case '*.tar.bz2'
+    		tar xjf $argv
+    	case '*.tar.gz'
+    		tar xzf $argv
+    	case '*.bz2'
+    		bunzip2 $argv
+    	case '*.rar'
+    		unrar x $argv
+    	case '*.gz'
+    		gunzip $argv
+    	case '*.tar'
+    		tar xf $argv
+    	case '*.tbz2'
+    		tar xjf $argv
+    	case '*.tgz'
+    		tar xzf $argv
+    	case '*.zip'
+    		unzip $argv
+    	case '*.Z'
+    		uncompress $argv
+    	case '*.7z'
+    		7z x $argv
+    	case '*.deb'
+    		ar x $argv
+    	case '*.tar.xz'
+    		tar xf $argv
+    	case '*.tar.zst'
+    		unzstd $argv
+    	case '*'
+    		echo "'$argv' not a recognised archive type!" 
+    end
+  else
+    echo "'$argv' is not a valid file"
+  end
+end
